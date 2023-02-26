a5ae0bf4b17418c68a432506c61cc293d17ea441
Nicolas Aldao <nicolasaldao@Nicolass-MacBook-Pro.local>
2023-02-26 02:18:07 -0300

    deleted some test code

--- src/components/pages/HomeScreen/index.js
+++ src/components/pages/HomeScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Context as BlogContext } from '../../../context/AppContext';
 import { BasicLayout } from '../../layouts/BasicLayout';
@@ -6,15 +6,8 @@ import { ListOfPhotocards } from '../../container/ListOfPhotocards';
 
 export const HomeScreen = () => {
   const params = useParams();
-  const { state, getBlogPosts } = useContext(BlogContext);
 
   useEffect(() => {
-    try {
-      const res = getBlogPosts(); // first time we load this screen
-    } catch (err) {
-      //setMessageError('Ha ocurrido un error, reintenta más tarde.');
-      console.log('useEffect ' + err);
-    }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     //let listener: any = navigation.addListener('focus', () => {
     // every time this screen gets focus again
@@ -31,15 +24,6 @@ export const HomeScreen = () => {
       title="Tu app de fotos de mascotas"
       subtitle="Con Petgram puedes encontrar fotos de animales domésticos muy bonitos"
       showCategories>
-      {state.length > 0 &&
-        state.map(blog => {
-          return (
-            <div key={blog.title}>
-              <p >{blog.title}</p>
-              <p>{blog.content}</p>
-            </div>
-          );
-        })}
       <ListOfPhotocards categoryId={params.id} />
     </BasicLayout>
   );
